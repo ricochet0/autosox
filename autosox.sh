9e9074d1281ba0e0e2046657158d85a901e8ac56
= <c.tollef@protonmail.com>
2021-01-27 02:58:56 +0100

    display tree structure in output

--- autosox.sh
+++ autosox.sh
@@ -16,33 +16,34 @@ filetype_patterns="+(*.flac|*.mp3|*.wav|*.ape)"
 traverse() {
 
     local -r path=$1
-    echo "==> $(basename  $path)"
+    local -r arrow=$2
+    echo "$arrow $(basename $path)"
     mkdir -p $out_dir/$path
 
     for entry in $(ls $path); do
 
         # exclude irrelevant patterns and files already sox-ed
-        if [[ $path/$entry == $filetype_patterns ]] && [[ ! -f "$out_dir/$path/$entry.jpg" ]]; then
+        if [[ $path/$entry == $filetype_patterns ]] && [[ ! -f $out_dir/$path/$entry.jpg ]]; then
             # sox it and compress the image
-            sox "$path/$entry" -n spectrogram -o "$out_dir/$path/$entry.png"
+            sox $path/$entry -n spectrogram -o "$out_dir/$path/$entry.png"
             ((scan_count++))
-            mogrify -strip -quality 80% -sampling-factor 4:4:4 -format jpg "$out_dir/$path/$entry.png"
-            rm -f "$out_dir/$path/$entry.png"
+            mogrify -strip -quality 80% -sampling-factor 4:4:4 -format jpg $out_dir/$path/$entry.png
+            rm -f $out_dir/$path/$entry.png
         fi
 
-        if [ -d "$path/$entry" ]; then
-            traverse "$path"/"$entry" # recursive call on subdir
+        if [ -d $path/$entry ]; then
+            traverse $path/$entry "====$arrow" # recursive call on subdir
         fi
     done
 
 }
 
 if [ $# != 1 ]; then
-    echo "Need one arg :)"
+    echo "Need one arg, given $#"
 else
     mkdir -p $out_dir
-    traverse $(echo "$1" | sed 's:/*$::') # strip trailing backslashes
-    echo -e "\nYou have $scan_count new spectrograms to look at :-)"
+    traverse $(echo "$1" | sed 's:/*$::') "===>" # strip trailing backslashes
+    echo -e "\nYou have $scan_count new spectrograms"
 fi
 
 IFS=$SAVEIFS
